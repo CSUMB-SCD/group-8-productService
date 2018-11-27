d13cccabab3992afefa239e4aae76a4c2a7cc248
Alexander Paulsell <alexander.dean.paulsell@gmail.com>
2018-11-26 21:40:15 -0800

    new path

--- src/main/java/edu/csumb/cst438/productservice/data/ProductDbClient.java
+++ src/main/java/edu/csumb/cst438/productservice/data/ProductDbClient.java
@@ -13,7 +13,7 @@ import edu.csumb.cst438.productservice.api.products.Product;
 public class ProductDbClient {
     
     public List<Product> getAll(){
-        final String uri = "https://proj-zuul.herokuapp.com/prod/Product/";
+        final String uri = "https://proj-zuul.herokuapp.com/productdb/Product/";
 
         RestTemplate restTemplate = new RestTemplate();
         List<Product> result = restTemplate.getForObject(uri, ArrayList.class);
