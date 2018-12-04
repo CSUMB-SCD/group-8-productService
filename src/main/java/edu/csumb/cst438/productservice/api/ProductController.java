6cc0125158220c8f06f38ff26ed6178755554c4e
Alexander Paulsell <alexander.dean.paulsell@gmail.com>
2018-12-03 23:07:13 -0800

    hirez to service

--- src/main/java/edu/csumb/cst438/productservice/api/ProductController.java
+++ src/main/java/edu/csumb/cst438/productservice/api/ProductController.java
@@ -7,10 +7,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import edu.csumb.cst438.productservice.api.products.Payload;
 import edu.csumb.cst438.productservice.api.products.Product;
 import edu.csumb.cst438.productservice.business.Manager;
 
@@ -25,13 +29,7 @@ public class ProductController {
     List<Product> getProducts(){
         return manager.getProductList();
     }
-
-    @GetMapping ("/test")
-    @ResponseBody
-    String getTestString(){
-        return "test";
-    }
-
+    
     @GetMapping("/")
     public String home() {
         return "This is a trivial service that demonstrates how a Eureka Client can register with a Eureka Server";
